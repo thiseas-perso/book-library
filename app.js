2f442c4403204a7c9586a796c35e0b743969855d
thiseaskambas <tkambas@yahoo.com>
2022-02-04 16:03:46 -0400

    js selectors

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 
 
+const form = document.querySelector('#form')
+const title = document.querySelector('#title')
+const pages = document.querySelector('#pages')
+const author = document.querySelector('#author')
+const read = document.querySelector('#read')
 
 
 const myLibrary = [];
@@ -14,8 +19,20 @@ function Book(title, author, pages, read) {
   }
 }
 
-const lotr = new Book("The Lord of The Rings", "JR Tolkien", 1200, 'not yet')
 
-function addBookToLibrary(book) {
+
+function addBookToLibrary() {
+  let bookTitle = title.value
+  let bookAuthor = author.value
+  let bookRead = read.checked
+  let book = new Book(bookTitle, bookAuthor, bookPages, bookRead)
   myLibrary.push(book)
 }
+
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  addBookToLibrary()
+})
+
