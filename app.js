279f5098ad76cbd25fc4bfe3b1d7add3f6e7df01
thiseaskambas <tkambas@yahoo.com>
2022-02-04 17:58:51 -0400

    js event listeners

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 
 
 const form = document.querySelector('#form')
+const formTitle = document.querySelector('#form-title')
 const title = document.querySelector('#title')
 const pages = document.querySelector('#pages')
 const author = document.querySelector('#author')
 const read = document.querySelector('#read')
+const newButton = document.querySelector('#new-book')
 
 
 const myLibrary = [];
@@ -19,17 +21,39 @@ function Book(title, author, pages, read) {
   }
 }
 
+function submitValidation() {
+  const validateBanner = document.createElement('div')
+  const validateText = document.createElement('p')
+  const closeBanner = document.createElement('span')
+  closeBanner.innerText = "cancel"
+  closeBanner.setAttribute('class', 'material-icons-outlined close')
+  closeBanner.setAttribute('id', 'close')
+  closeBanner.addEventListener('click', (e) => {
+    form.removeChild(validateBanner)
+  })
+  validateBanner.setAttribute('class', 'valid')
+  validateBanner.appendChild(validateText)
 
+  validateText.innerText = "Book added!"
+  validateBanner.appendChild(closeBanner)
+
+  form.insertBefore(validateBanner, formTitle)
+}
 
 function addBookToLibrary() {
   let bookTitle = title.value
   let bookAuthor = author.value
   let bookRead = read.checked
+  let bookPages = pages.value
   let book = new Book(bookTitle, bookAuthor, bookPages, bookRead)
   myLibrary.push(book)
+  form.reset()
+  submitValidation()
 }
 
-
+newButton.addEventListener('click', (e) => {
+  form.setAttribute('class', 'after-form')
+})
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
