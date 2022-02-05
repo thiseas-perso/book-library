55c493cffa2254d31a3b817aa19d6c761e253428
thiseaskambas <tkambas@yahoo.com>
2022-02-04 21:32:45 -0400

    completed

--- app.js
+++ app.js
@@ -19,35 +19,7 @@ function Book(title, author, pages, read) {
   this.read = read
 }
 
-//////seed page and array/////
-const lotr = new Book('LOTR', 'Tolkien', 500, true)
-const hpOne = new Book('HarryPotter 1', 'JKR', 300, true)
-const hpTwo = new Book('HarryPotter 2', 'JKR', 400, true)
-myLibrary.push(lotr)
-myLibrary.push(hpOne)
-myLibrary.push(hpTwo)
 
-function displayBooks() {
-  for (let i = 0; i < myLibrary.length; i++) {
-    let cardTitle = document.createElement('h2')
-    let cardAuthor = document.createElement('p')
-    let cardPages = document.createElement('p')
-    let cardRead = document.createElement('div')
-    let card = document.createElement('div')
-    card.setAttribute('class', 'card')
-    card.setAttribute('id', `card${i}`)
-    cardTitle.innerText = `${myLibrary[i].title}`
-    cardAuthor.innerText = `${myLibrary[i].author}`
-    cardPages.innerText = `${myLibrary[i].pages} pages`
-    card.appendChild(cardTitle)
-    card.appendChild(cardAuthor)
-    card.appendChild(cardPages)
-    library.appendChild(card)
-  }
-}
-
-displayBooks()
-///////////
 
 let banner = false;
 
@@ -95,22 +67,120 @@ form.addEventListener('submit', (e) => {
 })
 
 
-
-
-
 function addBookToPage(book) {
   let cardTitle = document.createElement('h2')
   let cardAuthor = document.createElement('p')
   let cardPages = document.createElement('p')
-  let cardRead = document.createElement('div')
+  let cardRead = document.createElement('button')
   let card = document.createElement('div')
+
+  const dltBook = document.createElement('span')
+  dltBook.innerText = "cancel"
+  dltBook.setAttribute('class', 'material-icons-outlined dlt')
+  dltBook.setAttribute('id', 'dlt')
+  dltBook.addEventListener('click', (e) => {
+    let index = e.target.parentElement.id.slice(4)
+
+    library.removeChild(e.target.parentElement)
+    myLibrary.splice(index, 1)
+  })
+
+  if (book.read) {
+    cardRead.setAttribute('id', 'btn-read')
+    cardRead.innerText = 'read'
+  } else {
+    cardRead.setAttribute('id', 'btn-not-read')
+    cardRead.innerText = 'not read'
+  }
+
+  cardRead.addEventListener('click', toggleRead)
+
   card.setAttribute('class', 'card')
+  card.setAttribute('id', `book${myLibrary.length - 1}`)
   cardTitle.innerText = `${book.title}`
   cardAuthor.innerText = `${book.author}`
   cardPages.innerText = `${book.pages} pages`
   card.appendChild(cardTitle)
+  card.appendChild(dltBook)
   card.appendChild(cardAuthor)
   card.appendChild(cardPages)
+  card.appendChild(cardRead)
   library.appendChild(card)
 }
 
+
+
+function toggleRead(e) {
+  let index = e.target.parentElement.id.slice(4)
+  console.log(index)
+  if (myLibrary[index].read) {
+    myLibrary[index].read = false
+    e.target.id = "btn-not-read"
+    e.target.innerText = "not read yet"
+  } else {
+    myLibrary[index].read = true
+    e.target.id = "btn-read"
+    e.target.innerText = "read"
+  }
+}
+
+
+
+
+
+////////////////////////////////////////////
+//////seed page and array/////
+const lotr = new Book('LOTR', 'Tolkien', 500, true)
+const hpOne = new Book('HarryPotter 1', 'JKR', 300, true)
+const hpTwo = new Book('HarryPotter 2', 'JKR', 400, true)
+myLibrary.push(lotr)
+myLibrary.push(hpOne)
+myLibrary.push(hpTwo)
+
+function displayBooks() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    let cardTitle = document.createElement('h2')
+    let cardAuthor = document.createElement('p')
+    let cardPages = document.createElement('p')
+    let cardRead = document.createElement('button')
+    let card = document.createElement('div')
+
+
+
+    const dltBook = document.createElement('span')
+    dltBook.innerText = "cancel"
+    dltBook.setAttribute('class', 'material-icons-outlined dlt')
+    dltBook.setAttribute('id', 'dlt')
+    dltBook.addEventListener('click', (e) => {
+      let index = e.target.parentElement.id.slice(4)
+
+      library.removeChild(e.target.parentElement)
+      myLibrary.splice(index, 1)
+    })
+
+    card.setAttribute('class', 'card')
+    card.setAttribute('id', `book${i}`)
+    cardTitle.innerText = `${myLibrary[i].title}`
+    cardAuthor.innerText = `${myLibrary[i].author}`
+    cardPages.innerText = `${myLibrary[i].pages} pages`
+
+    if (myLibrary[i].read) {
+      cardRead.setAttribute('id', 'btn-read')
+      cardRead.innerText = 'read'
+    } else {
+      cardRead.setAttribute('id', 'btn-not-read')
+      cardRead.innerText = 'not read'
+    }
+    cardRead.addEventListener('click', toggleRead)
+
+    card.appendChild(cardTitle)
+    card.appendChild(dltBook)
+    card.appendChild(cardAuthor)
+    card.appendChild(cardPages)
+    card.appendChild(cardRead)
+    library.appendChild(card)
+  }
+}
+
+displayBooks()
+///////////
